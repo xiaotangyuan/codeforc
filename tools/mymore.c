6c527614a25442e03dc38e0af6757989423a711f
timluo <timluo@example.com>
2016-03-24 07:41:45 -0700

    fgets line in filename

--- tools/mymore.c
+++ tools/mymore.c
@@ -1,10 +1,28 @@
 /*
  * diy more
  */
-#include <stdio>
+#include <stdio.h>
+int action_more();
 
 int main(int argc, char *argv[]){
-    FILE *fp;
+	FILE  *fp;
+	while(--argc){
+		// argv 为指针，++argv为下一个参数字符串的指针，* ++argv 取到下一个字符串值，如：test.c
+		fp=fopen(* ++argv,"r");
+		if(fp != NULL){
+			action_more(fp);
+		}
+	}
 
 
+}
+
+
+int action_more(FILE *fp){
+	//执行more显示内容	
+	char line[512];
+	while(fgets(line,512,fp)){
+		printf("%s",line);
+	}	
+	
 }
